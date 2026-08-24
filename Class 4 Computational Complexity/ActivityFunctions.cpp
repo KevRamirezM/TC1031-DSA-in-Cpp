5a7244207e735b0addef108223fd20e1b570e298
KevRamirezM <kevinkapi20@gmail.com>
2026-08-24 10:57:06 -0600

    Class 4 exercises and notes

---
+++ Class 4 Computational Complexity/ActivityFunctions.cpp
@@ -0,0 +1,79 @@
+// A00844255
+#include <iostream>
+using namespace std;
+
+int iterativeSum(int n){
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+};
+
+int recursiveSum(int n){
+    if(n == 0){
+        return 0;
+    }
+    return n + recursiveSum(n - 1);
+}
+
+int directSum(int n){
+    return (n * (n + 1)) / 2;
+}
+
+int main(){
+    int num;
+    cout << "Choose an opcion" << endl;
+    cout << "1 iterativeSum" << endl;
+    cout << "2 recursiveSum" << endl;
+    cout << "3 directSum" << endl;
+    cin >> num;
+    switch(num){
+        case 1:{
+            int number;
+            int result;
+            cout << "Insert a number ";
+            cin >> number;
+            if (number>0){
+                result = iterativeSum(number);
+                cout << "Result " << result;
+            }
+            else{
+                cout << "Negative number is not valid";
+            }
+        break;
+        }
+        case 2:{
+            int number;
+            int result;
+            cout << "Insert a number ";
+            cin >> number;
+            if (number>0){
+                result = recursiveSum(number);
+                cout << "Result " << result;
+            }
+            else{
+                cout << "Negative number is not valid";
+            }
+        break;
+        }
+        case 3:{
+            int number;
+            int result;
+            cout << "Insert a number ";
+            cin >> number;
+            if (number>0){
+                result = directSum(number);
+                cout << "Result " << result << endl;
+            }
+            else{
+                cout << "Negative number is not valid";
+            }
+        break;
+        }
+        default:{
+            cout << "Invalid request";
+            break;
+        }
+    }
+};
---
+++ Class 4 Computational Complexity/Notes.md
@@ -0,0 +1,33 @@
+# Computational Complexity
+
+Measure of the amount of computing resources, such as time and memory, that an algorithm requires to run as a function of the input size.
+
+## P Problems
+
+These are problems that can be solved and verified in polynomial time. This is a complexity class.
+
+Some examples of this types of problems in Big O notation are:
+
+- O(n)
+- O(n^2)
+- O(n^3)
+
+## NP Problems (Nondeterministic Polynomial)
+
+These are problems that can be verified in polynomial time but may be difficult to solve (for a computer). P is a subclass of NP. This is a complexity class.
+
+Some examples of this types of problems in Big O notation are:
+
+- O(2^n)
+- O(n!)
+- O(n^k)
+
+## NP-Hard Problems
+
+These are problems that are at least as hard as the hardest problems in NP. If a polynomial-time algorithm exists for any NP-hard problem, then all problems in NP can be solved in polynomial time. This is a complexity class.
+
+## NP-Complete Problems
+
+These are problems that are both NP and NP-hard. If a polynomial-time algorithm exists for any NP-complete problem, then all problems in NP can be solved in polynomial time. They are the hardest problems in NP. This is a complexity class.
+
+
