c7d1fda50b004813debb0eeb31bbe8098220fe36
KevRamirezM <kevinkapi20@gmail.com>
2026-08-24 12:13:39 -0600

    Improved Class 4 Activity, finished.

--- Class 4 Computational Complexity/ActivityFunctions.cpp
+++ Class 4 Computational Complexity/ActivityFunctions.cpp
@@ -1,4 +1,7 @@
+// Activity 1.1 Iterative, Recursive Functions and their Complexity Analysis
 // A00844255
+// 24-08-2026
+
 #include <iostream>
 using namespace std;
 
@@ -30,6 +33,7 @@ int main(){
     cout << "1 iterativeSum" << endl;
     cout << "2 recursiveSum" << endl;
     cout << "3 directSum" << endl;
+    cout << "4 all of them" << endl;
     cin >> num;
     switch(num){
         case 1:{
@@ -68,6 +72,26 @@ int main(){
             if (number>=0){
                 result = directSum(number);
                 cout << "Result " << result << endl;
+            }
+            else{
+                cout << "Negative number is not valid";
+            }
+        break;
+        }
+        case 4:{
+            int number;
+            int result1;
+            int result2;
+            int result3;
+            cout << "Insert a number ";
+            cin >> number;
+            if (number>=0){
+                result1 = iterativeSum(number);
+                cout << "Result of iterativeSum: " << result1 << endl;
+                result2 = recursiveSum(number);
+                cout << "Result of recursiveSum: " << result2 << endl;
+                result3 = directSum(number);
+                cout << "Result of directSum: " << result3 << endl;
             }
             else{
                 cout << "Negative number is not valid";
