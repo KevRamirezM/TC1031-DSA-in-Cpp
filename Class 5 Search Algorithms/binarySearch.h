a3819562a238f11ee79f87c8b587ea5a734fb8ff
KevRamirezM <kevinkapi20@gmail.com>
2026-09-07 14:20:24 -0600

    Implemented Binary Search

--- Class 5 Search Algorithms/binarySearch.h
+++ Class 5 Search Algorithms/binarySearch.h
@@ -4,5 +4,20 @@ using namespace std;
 template <typename T>
 
 int binarySearch(const vector<T> &data, const T &target){
-    
+    int left = 0;
+    int right = data.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (data[mid] == target) {
+            return mid;
+        } else if (data[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1; // Target not found
 }
